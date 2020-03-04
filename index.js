1e403cf2f834acc4749497f350fb1c773ba4b8e5
FZxiao233 <fzxiao@outlook.com>
2020-03-04 22:31:16 +0800

    修复watch多次检测的错误 改为txt与srt共存 filename字段应来自api请求
    Signed-off-by: FZxiao233 <fzxiao@outlook.com>

--- index.js
+++ index.js
@@ -9,22 +9,25 @@ let currentLive = new Map();
 app.get('/api/live', function (req, res) {
     const roomId = parseInt(req.query.roomId);
     const status = parseInt(req.query.status);
-    console.log(roomId + ': ' + status);
-    watch({roomId, status});
-    res.send({'msg': 1});
+    const filename = req.query.filename;
+    if (roomId && filename) {
+        console.log(roomId + ': ' + status);
+        watch({roomId, status, filename});
+        res.send({'msg': 1});
+    }
 });
 app.listen(config.ExpressPort, function () {
     console.log(`listening on port ${config.ExpressPort}!`);
 });
-const watch = ({roomId, status}) => {
-    if (!currentLive[roomId] && status == 1) {
-        new LiveEvent(roomId).emit('start');
+const watch = ({roomId, status, filename}) => {
+    if (!currentLive[roomId] && status === 1) {
+        new LiveEvent(roomId, filename).emit('start');
     }
     currentLive[roomId] = status;
 };
 
 class LiveEvent extends EventEmitter {
-    constructor(roomId) {
+    constructor(roomId, filename) {
         super();
 
         this.roomId = roomId;
@@ -35,8 +38,9 @@ class LiveEvent extends EventEmitter {
         this.prevTime = "";
         this.prevText = "";
 
-        let path = `${config.DownloadDir}/${this.roomId}_${this.startTime}.srt`;
-        this.file = fs.createWriteStream(path, {flags: 'a+'});
+        this.path = `${config.DownloadDir}/${filename}`;
+        this.txtFile = fs.createWriteStream(this.path, {flags: 'a+'});
+        this.srtFile = fs.createWriteStream(this.path.split('.txt')[0] + '.srt', {flags: 'a+'});
 
         this.watch();
         this.on('start', () => {
@@ -44,58 +48,85 @@ class LiveEvent extends EventEmitter {
         });
         this.on('close', () => {
             this.writeSubtitle(null);
+            this.writePlainText(null);
             this.live.close();
         })
     }
 
     writeSubtitle(text) {
         let lastTime = Date.now() - this.startTime;
-        let h  = Math.floor(lastTime/1000/60/60);
-        let m  = Math.floor(lastTime/1000/60%60);
-        let s  = Math.floor(lastTime/1000%60);
-        let ms = Math.floor(lastTime%1000);
+        let h = Math.floor(lastTime / 1000 / 60 / 60);
+        let m = Math.floor(lastTime / 1000 / 60 % 60);
+        let s = Math.floor(lastTime / 1000 % 60);
+        let ms = Math.floor(lastTime % 1000);
 
-        let t1 = (time) => ((time<10)?'0':'')+time;
-        let t2 = (time) => ((time<10)?'00':(time<100)?'0':'')+time;
+        let t1 = (time) => ((time < 10) ? '0' : '') + time;
+        let t2 = (time) => ((time < 10) ? '00' : (time < 100) ? '0' : '') + time;
 
         let now = `${t1(h)}:${t1(m)}:${t1(s)}.${t2(ms)}`;
 
-        if (this.count != 0) {
-            this.file.write(`${this.count}\n${this.prevTime} --> ${now}\n${this.prevText}\n`);
+        if (this.count !== 0) {
+            try {
+                this.srtFile.write(`${this.count}\n${this.prevTime} --> ${now}\n${this.prevText}\n`);
+            } catch (e) {
+                //pass
+            }
+
         }
         this.count++;
 
         if (!text) {
-            this.file.close();
+            this.srtFile.close();
             return;
         }
         this.prevTime = now;
         this.prevText = text;
     }
 
+    writePlainText(s) {
+        if (!s) {
+            this.txtFile.close();
+            return
+        }
+        try {
+            this.txtFile.write(s + "\n")
+        } catch (e) {
+            //pass
+        }
+
+    }
+
     monitor() {
         this.live.on('open', () => console.log(this.roomId + 'Connection is established'));
         this.live.on('DANMU_MSG', (data) => {
             let danmakuText = data['info'][1];
-            let subtitle = this.danmakuFilter(danmakuText);
-            if (subtitle) this.writeSubtitle(subtitle);
+            console.log(danmakuText);
+            let s = this.danmakuFilter(danmakuText);
+            if (s) {
+                this.writePlainText(s);
+                this.writeSubtitle(s);
+            }
         });
     };
+
     watch() {
-        setInterval(() => {
+        const _watch = () => {
             if (currentLive[this.roomId] === 0) {
                 this.emit('close')
             } else {
                 console.log(`KeepAlive ${this.roomId}`)
             }
-        }, 3000);
+            setTimeout(_watch, 3000)
+        };
+        setTimeout(_watch, 3000)
     };
+
     danmakuFilter(raw) {
         let leftPos = raw.indexOf("【");
         let rightPos = raw.indexOf("】");
 
-        if (leftPos == -1 && rightPos == -1) return null;
-        if (leftPos != 0) raw = raw.replace("【", "：");
+        if (leftPos === -1 && rightPos === -1) return null;
+        if (leftPos !== 0) raw = raw.replace("【", "：");
 
         return raw.replace("【", "").replace("】", "");
     }
